e0a0150a35145470104d50a1c82657700a796b41
Владимир Ковалёв <v.a.kovalev@list.ru>
2023-09-12 22:49:15 +0300

    bjs-diplom

---
+++ public/loginPage.js
@@ -0,0 +1,8 @@
+"use strict";
+let user = new UserForm ();
+let hhr = new HtmlHttpRequest()
+user.loginFormCallback = requestToServer(data);
+function requestToServer(data) {
+  ApiConnector.login;
+  hhr.send();
+}
