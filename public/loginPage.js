eb7bcf57562ead203cb9e862a34f9f6abdc6cefc
Владимир Ковалёв <v.a.kovalev@list.ru>
2023-09-12 23:19:10 +0300

    bjs-diplom

--- public/loginPage.js
+++ public/loginPage.js
@@ -1,8 +1,21 @@
 "use strict";
-let user = new UserForm ();
-let hhr = new HtmlHttpRequest()
-user.loginFormCallback = requestToServer(data);
-function requestToServer(data) {
+let user = new UserForm();
+let hhr = new HtmlHttpRequest();
+user.loginFormCallback = authorizationRequest(data);
+function authorizationRequest(data) {
   ApiConnector.login;
-  hhr.send();
+  if (hhr.send()) {
+    hhr.onload = callback;
+    return location.reload();
+  }
+  alert("ошибка авторизации");
+}
+user.registerFormCallback = registrationRequest(data);
+function registrationRequest(data) {
+  ApiConnector.register;
+  if (hhr.send()) {
+    hhr.onload = callback;
+    return location.reload();
+  }
+  alert("ошибка регистрации");
 }
