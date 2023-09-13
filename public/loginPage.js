7ff958301d86b07aadf39d8e31c5afa33bc589ff
Владимир Ковалёв <v.a.kovalev@list.ru>
2023-09-13 21:16:08 +0300

    bjs-diplom

--- public/loginPage.js
+++ public/loginPage.js
@@ -3,8 +3,7 @@ let user = new UserForm();
 let hhr = new HtmlHttpRequest();
 user.loginFormCallback = authorizationRequest(data);
 function authorizationRequest(data) {
-  ApiConnector.login;
-  login({ login, password }, callback);
+  ApiConnector.login({ login, password }, callback);
   if (hhr.send()) {
     hhr.onload = callback;
     return location.reload();
