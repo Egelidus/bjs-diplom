1ae9e426529ce39230290bb3cab56252c66e95d4
Владимир Ковалёв <v.a.kovalev@list.ru>
2023-09-13 22:08:27 +0300

    bjs-diplom

--- public/loginPage.js
+++ public/loginPage.js
@@ -1,12 +1,36 @@
-"use strict";
-let user = new UserForm();
-user.loginFormCallback = (data) => {
-  ApiConnector.login({ login, password }, callback)
-    ? location.reload()
-    : user.setLoginErrorMessage("ошибка авторизации");
-};
-user.registerFormCallback = (data) => {
-  ApiConnector.register({ login, password }, callback)
-    ? location.reload()
-    : user.setLoginErrorMessage("ошибка регистрации");
-};
+// "use strict";
+// let user = new UserForm();
+// user.loginFormCallback = (data) => {
+//   ApiConnector.login({ login, password }, callback)
+//     ? location.reload()
+//     : user.setLoginErrorMessage("ошибка авторизации");
+// };
+// user.registerFormCallback = (data) => {
+//   ApiConnector.register({ login, password }, callback)
+//     ? location.reload()
+//     : user.setLoginErrorMessage("ошибка регистрации");
+// };
+
+"use strict"
+
+const userForm = new UserForm();
+// Авторизация
+userForm.loginFormCallback = (data = {login: null, password: null}) => {
+  ApiConnector.login(data, (response)=>{
+    if (response.success) {
+      location.reload();
+    } else {
+      alert(response.data);
+    }
+  })
+}
+// Регистрация
+userForm.registerFormCallback = (data = {login: null, password: null}) => {
+  ApiConnector.register(data, (response)=>{
+    if (response.success) {
+      location.reload();
+    } else {
+      alert(response.data);
+    }
+  })
+}
